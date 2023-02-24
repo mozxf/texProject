3b91fd1d94aa29627b9ecd5b0f7a33d7fcb17c0f
mozxf <nandogustavo2001*gmail.com>
2023-02-24 12:48:39 -0300

    adding envs

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBwTohBrXB2mVACmOZTSzz5l_ohn0UMj68',
+    authDomain: 'crud-angular-firebase-eeb48.firebaseapp.com',
+    databaseURL:
+      'https://crud-angular-firebase-eeb48-default-rtdb.firebaseio.com',
+    projectId: 'crud-angular-firebase-eeb48',
+    storageBucket: 'crud-angular-firebase-eeb48.appspot.com',
+    messagingSenderId: '1023000113323',
+    appId: '1:1023000113323:web:baf0e6e82effaf59aa47ee',
+    measurementId: 'G-BZNLH1JBVV',
+  },
+};
